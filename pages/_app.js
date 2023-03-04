8bd7f5ff073cf46e1291810190382af797048356
placeparks <placeparks@gmail.com>
2023-03-04 23:45:07 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = ChainId.Mumbai;
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,57 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useAddress, Web3Button } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
+const dropAddress = "0xF943975a84Ca4881f2F2D7ab2fb3421CA137aF9F";
+
 export default function Home() {
+  const address = useAddress();
+
+  const triggerMail = async () => {
+    try {
+      const response = await fetch("/api/send-message", {
+        method: "POST",
+        body: JSON.stringify({ address }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        console.log(data);
+        return alert(
+          "Unable to send mail! However, the drop has been claimed!"
+        );
+      }
+      alert(
+        "Drop claimed successfully! Check your mailchain for further instructions!"
+      );
+    } catch (err) {
+      console.error(err);
+      alert(
+        "An error occurred while sending the mail, however the drop has been claimed successfully"
+      );
+    }
+  };
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
+        <h1 className={styles.title}>Claim our drop!</h1>
 
         <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.js</code> file!
+          Claim the drop using the button below and we will send email through Mailchain!
         </p>
 
         <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
+          <Web3Button
+            contractAddress={dropAddress}
+            action={async (contract) => {
+              //await contract.erc721.claim(1);
+              await triggerMail();
+            }}
           >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+            Claim Drop!
+          </Web3Button>
         </div>
       </main>
     </div>
